3d1f343277ed0925c874528426e0e2de619da660
CódigoByte <maquino@codigobyte.com.ar>
2023-06-30 19:21:16 -0300

    Se agrego la automatizacion de las fechas en el kernel del modelo ConfiguracionMatrcilas

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Models\ConfiguracionMatricula;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,8 +14,49 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            // Obtener el primer registro de ConfiguracionMatricula
+            $configuracionMatricula = ConfiguracionMatricula::first();
+    
+            if ($configuracionMatricula) {
+                // Obtener el mes actual y el año actual
+                $mesActual = Carbon::now()->format('m');
+                $anioActual = Carbon::now()->format('Y');
+                
+                // Obtener el día de vencimiento y el día de segundo vencimiento
+                $diaVencimiento = Carbon::parse($configuracionMatricula->fecha_vencimiento)->format('d');
+                $diaSegundoVencimiento = Carbon::parse($configuracionMatricula->fecha_segundo_vencimiento)->format('d');
+    
+                // Construir nuevas fechas de vencimiento y segundo vencimiento
+                $configuracionMatricula->fecha_vencimiento = Carbon::parse($anioActual.'-'.$mesActual.'-'.$diaVencimiento)->toDateString();
+                $configuracionMatricula->fecha_segundo_vencimiento = Carbon::parse($anioActual.'-'.$mesActual.'-'.$diaSegundoVencimiento)->toDateString();
+    
+                // Guardar los cambios en la base de datos
+                $configuracionMatricula->save();
+            }
+            /* Ejecuta la tarea una vez al día a las 00:00 (medianoche) */
+        })->daily();
+        /* 
+        Otras opciones de daily()
+        
+        En Laravel, el método everyMinute() se utiliza para programar una tarea que se ejecuta cada minuto. Sin embargo, hay varias opciones adicionales disponibles para programar tareas en diferentes intervalos. A continuación, te muestro algunas de las opciones más comunes:
+
+        everyFiveMinutes(): Ejecuta la tarea cada 5 minutos.
+        everyTenMinutes(): Ejecuta la tarea cada 10 minutos.
+        everyThirtyMinutes(): Ejecuta la tarea cada 30 minutos.
+        hourly(): Ejecuta la tarea cada hora.
+        hourlyAt(): Ejecuta la tarea cada hora en un minuto específico (por ejemplo, hourlyAt(15) ejecuta la tarea en el minuto 15 de cada hora).
+        daily(): Ejecuta la tarea una vez al día a las 00:00 (medianoche).
+        dailyAt(): Ejecuta la tarea una vez al día en un horario específico (por ejemplo, dailyAt('13:00') ejecuta la tarea todos los días a la 1:00 PM).
+        twiceDaily(): Ejecuta la tarea dos veces al día, por defecto a las 00:00 y a las 12:00 (mediodía).
+        weekly(): Ejecuta la tarea una vez a la semana los lunes a las 00:00 (medianoche).
+        monthly(): Ejecuta la tarea una vez al mes el primer día del mes a las 00:00 (medianoche).
+        monthlyOn(): Ejecuta la tarea una vez al mes en un día específico y a una hora específica (por ejemplo, monthlyOn(15, '13:00') ejecuta la tarea el día 15 de cada mes a la 1:00 PM).
+        quarterly(): Ejecuta la tarea cada trimestre el primer día del trimestre a las 00:00 (medianoche).
+        yearly(): Ejecuta la tarea una vez al año el 1 de enero a las 00:00 (medianoche).
+        */
     }
+
 
     /**
      * Register the commands for the application.
--- app/Models/ConfiguracionMatricula.php
+++ app/Models/ConfiguracionMatricula.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ConfiguracionMatricula extends Model
 {
     use HasFactory;
-    
+
+    protected $table = 'configuracion_matriculas';
+        
     protected $fillable = ['precio_matricula', 'fecha_vencimiento', 'fecha_segundo_vencimiento', 'recargo_despues_vencimiento'];
 }
