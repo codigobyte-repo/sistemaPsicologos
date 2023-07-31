252bfe55c9d3e25ded71164f58ae8f7499eb53ac
Martin Aquino <maquino@codigobyte.com.ar>
2023-07-31 16:58:47 -0300

    Se agrego al menu sidebar-collapsable el link a datos personales hacia el user profile

--- resources/views/livewire/admin/sidebar-collapsable.blade.php
+++ resources/views/livewire/admin/sidebar-collapsable.blade.php
@@ -103,7 +103,7 @@
             @endcan
         </li>
         <li>
-            <a href="#" class="flex items-center space-x-3 text-gray-300 p-2 rounded-md font-medium hover:bg-gray-600 focus:bg-gray-800 focus:shadow-outline">
+            <a href="{{ url('user/profile') }}" class="flex items-center space-x-3 text-gray-300 p-2 rounded-md font-medium hover:bg-gray-600 focus:bg-gray-800 focus:shadow-outline">
                 <span class="text-gray-300">
                     <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
