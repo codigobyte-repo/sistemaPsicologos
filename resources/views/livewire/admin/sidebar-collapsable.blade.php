58124d51e620b386df019a7c70304f3060968fca
Martin Aquino <maquino@codigobyte.com.ar>
2023-12-18 16:48:48 -0300

    Se soluciono problema con la url roles

--- resources/views/livewire/admin/sidebar-collapsable.blade.php
+++ resources/views/livewire/admin/sidebar-collapsable.blade.php
@@ -79,7 +79,7 @@
         <div class="border-b border-gray-100 py-10"></div>
         
         <li>
-            <a href="{{ url('admin/roles/index') }}" class="flex items-center space-x-3 text-gray-300 p-2 rounded-md font-medium hover:bg-gray-600 focus:bg-gray-800 focus:shadow-outline">
+            <a href="{{ url('admin/roles') }}" class="flex items-center space-x-3 text-gray-300 p-2 rounded-md font-medium hover:bg-gray-600 focus:bg-gray-800 focus:shadow-outline">
                 <span class="text-gray-300">
                     <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
